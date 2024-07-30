6f66845ebbce0bef9671dd2a4debf38f0b077ec6
David S. <neojohn2312@gmail.com>
2024-07-29 19:10:21 -0500

    3rd-commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import { connect } from 'react-redux';
 import GifList from "./components/GifList";
 import GifFrom from "./components/GifForm";
-
+//19:52
 function App() {
   return (
     <div className="App">
-      Async Redux Project
+      <h1>Async Redux Project</h1>
     </div>
   );
 }
